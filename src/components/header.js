19fa68c5fd71bc99a42cd1997f7b71c53afa18d8
VladislavArkavenko <vladislav.arkavenko0097@gmail.com>
2019-04-23 18:02:30 +0300

    Add bg-image change logic and optimization. Add some text for layout.

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Header = () => (
-  <header />
+  <div className="header">
+    <div>
+      <h2> Skillshape, </h2>
+      <h3> your path revealed </h3>
+    </div>
+    <div className="menu">
+      <div className="burger">
+        <span />
+        <span />
+        <span />
+      </div>
+      <p> menu </p>
+    </div>
+  </div>
 );
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
+import { graphql, StaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -6,11 +9,88 @@ import SEO from '../components/seo';
 import 'normalize.css';
 import '../styles/main.scss';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Skillshape Landing Page" />
-    <h1> Hello world! </h1>
-  </Layout>
-);
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.count = this.count.bind(this);
+  }
+
+  componentDidMount() {
+    setInterval(this.count, 6000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.count);
+  }
+
+  count() {
+    this.setState(state => ({
+      counter: state.counter + 1,
+    }));
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <Layout>
+        <SEO title="Skillshape Landing Page" />
+        <StaticQuery
+          query={graphql`
+            query {
+              allFile(filter:{extension:{regex:"/(jpeg|jpg|gif|png)/"},  sourceInstanceName:{eq:"images"}}) {
+                edges {
+                  node {
+                    childImageSharp {
+                      fluid(quality: 100, maxWidth: 1920, maxHeight: 1080) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `}
+          render={data => (
+            <div className="bg-container">
+              {data.allFile.edges.map(({ node }, index, arr) => (
+                <Fade when={counter % arr.length === index}>
+                  <Img
+                    fluid={node.childImageSharp.fluid}
+                    alt="Background"
+                  />
+                </Fade>
+              ))}
+            </div>
+          )}
+        />
+        <div className="main-text">
+          <h2>
+            A platform for
+            <span> institutes  </span>
+            that make the world a
+          </h2>
+          <h1>
+            <span> Strong </span>
+            Place.
+          </h1>
+          <div className="buttons-wrapper">
+            <button>I ama student </button>
+            <button> Sign up as a school </button>
+          </div>
+          <p>
+            Hurry up!
+            <span> It's free </span>
+            for a limited time.
+          </p>
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
